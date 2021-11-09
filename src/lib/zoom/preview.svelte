b9e805b5b1d8896dc3dfddf17efee358a49cb6ef
cliff <cliff.wong@ehlacademy.org>
2021-11-09 12:17:36 +0800

    fix

--- src/lib/zoom/preview.svelte
+++ src/lib/zoom/preview.svelte
@@ -1,7 +1,6 @@
 <script>
 	export let item
 	import dayjs from "dayjs";
-	import {locale} from 'svelte-i18n'
 </script>
 
 <a href="/zoom/{item.rc_type}-{item.tutor_group_id}" class="flex">
@@ -10,7 +9,7 @@
 	</div>
 	<div class="ml-2 md:ml-4 flex flex-col sm:p-2">
 		<p class="text-blue-500 text-xs sm:text-sm sm:text">{item.rc_tag}</p>
-		<p class="font-bold text-sm sm:text">{$locale === 'hk' ? item.name_alter : item.name}</p>
+		<p class="font-bold text-sm sm:text">{item.name}</p>
 		<p class="text-gray-500 text-xs sm:text-sm mt-2">{item.r_level}</p>
 		<p class="text-gray-500 text-xs sm:text-sm">{item.tutor_name}</p>
 		<div class="mt-auto">
