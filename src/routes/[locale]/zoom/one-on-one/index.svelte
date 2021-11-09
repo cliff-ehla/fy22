8c4fab43622d1684b0a9d0ac3679c92200c8a6d3
cliff <cliff.wong@ehlacademy.org>
2021-11-09 15:45:44 +0800

    start to build static ad pages inside one-on-one directory

---
+++ src/routes/[locale]/zoom/one-on-one/index.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+	const ad_list = [
+		{
+			title_en: 'Story telling and speaking',
+			title_hk: '英文故事閱讀與會話課堂'
+		}
+	]
+</script>
+
+<div class="p-4 mx-auto max-w-screen-lg">
+	<div class="grid grid-cols-1 gap-4">
+		{#each ad_list as item}
+			<div class="mb-4">
+<!--				<img class="rounded-sm" src={item.thumbnail_path} alt={item.name}>-->
+				<a href="one-on-one/story-telling-and-speaking" class="mt-2 text-blue-500 font-bold text-t1">{item.title_hk}</a>
+			</div>
+		{/each}
+	</div>
+</div>
---
+++ src/routes/[locale]/zoom/one-on-one/story-telling-and-speaking.svelte
@@ -0,0 +1,13 @@
+<div class="p-4">
+	<div class="mb-4">
+		<h1 class="text-t1 font-bold">英文故事閱讀與會話課堂</h1>
+		<p>(L1-L4)</p>
+	</div>
+
+	<p>閱讀與英語會話是英文重要基礎! Zoom課堂提供了不可多得的純正英語環境及機會給孩子，在故事閱讀教學之中學習與外籍老師實時互動，不知不覺提升英文閱讀、聆聽和說話能力。這是香港本地老師和父母難以做到的!</p>
+
+	<h2>分享</h2>
+	<p>❝一個同外籍老師學英文，聽及講咗1年英文的小朋友，與另一個無同等經歷的小朋友，分別是什麼？❞</p>
+	以下是視頻解答：
+	<video controls src="https://ehla-media-bucket.s3.ap-southeast-1.amazonaws.com/website2021/story-telling.mp4"></video>
+</div>
