5fd145a33b638dabb4a0cc03b7075829fef4898f
cliff <cliff.wong@ehlacademy.org>
2021-11-10 16:49:51 +0800

    wip

--- src/routes/[locale]/zoom/one-on-one/reading-and-speaking-for-kindergarten.svelte
+++ src/routes/[locale]/zoom/one-on-one/reading-and-speaking-for-kindergarten.svelte
@@ -2,8 +2,14 @@
 	export const prerender = true;
 </script>
 
+<script>
+	import {page} from '$app/stores'
+	import WhatsappEnquiry from '$lib/zoom/one-on-one/whatsapp-enquiry.svelte'
+</script>
+
 <div class="mx-auto p-4 max-w-screen-lg">
 	<h1 class="text-t1">小精英級K班課程</h1>
+	<img class="rounded my-4" src="https://ehla-media-bucket.s3.ap-southeast-1.amazonaws.com/website2021/one-on-one/{$page.path.split('/').pop()}.jpg" alt="banner">
 	<p>閱讀與英語會話是英文重要基礎! 3至6歲是語言學習一個極為重要的階段。課程提供了不可多得的純正英語環境及機會給孩子，在閱讀教學之中學習與外籍老師實時互動，不知不覺提升英文閱讀和說話能力。這是香港本地老師和父母難以做到的!</p>
 	<p>🎯 外籍老師在小故事閱讀及對話主題教學之中，也會延伸教授內容及語境中的相關詞彙、Sentence pattern(句式)、朗讀、發音及表達。過程當中也會不時滲入日常對話教學，引導小朋友to speak like a native.</p>
 	<p>🎯 對K2, K3小朋友，課程亦會加入與升小一面試Interview 相關的重要內容。</p>
@@ -19,7 +25,7 @@
 	<p>*英小或國際學校特別要求小朋友具備相當不錯的英文口語能力</p>
 
 	<div class="bg-yellow-100 p-2 my-4">
-		<div class="text-blue-500">
+		<div class="font-bold text-blue-500">
 			❝一個同外籍老師學英文，聽及講咗1年英文的小朋友，與另一個無同等經歷的小朋友，分別是什麼？❞
 		</div>
 		<video class="max-w-full max-w-xl" controls src="https://ehla-media-bucket.s3.ap-southeast-1.amazonaws.com/website2021/story-telling.mp4"></video>
@@ -28,16 +34,16 @@
 	<div class="bg-blue-100 p-2 my-4">
 		<div class="my-4">
 			<h2>重要課後溫習鞏固環節</h2>
-			<p>極為獨特的課後溫習鞏固環節, 對小朋友英文提升效果超乎你想像</p>
+			<p>極為獨特的課後溫習鞏固環節, 對小朋友英文提升效果超乎你想像❗</p>
 			<video controls src="https://ehla-media-bucket.s3.ap-southeast-1.amazonaws.com/website2021/powerful-solution/video3.mp4"></video>
 		</div>
 	</div>
 
-	<div class="p-4 bg-gray-200">
+	<div class="p-4 bg-gray-200 rounded">
 		<p>對象學生： K2-K3</p>
-		<p>歡迎試堂: 1對1: 30分鐘</p>
-		<p>試堂老師 = 正式課堂老師 (固定)</p>
-		<p>查詢 9999-9999</p>
+		<p>形式：一對一 Zoom</p>
+		<p>課時: 每節課30分鐘</p>
+		<WhatsappEnquiry/>
 	</div>
 </div>
 
