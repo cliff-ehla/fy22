ab1f654e301c13b229a8a2cb2c032ce66be69203
cliff <cliff.wong@ehlacademy.org>
2021-11-11 10:46:26 +0800

    wip

--- src/routes/[locale]/zoom/one-on-one/english-language-leap-programme.svelte
+++ src/routes/[locale]/zoom/one-on-one/english-language-leap-programme.svelte
@@ -3,32 +3,45 @@
 </script>
 
 <script>
+	import {_, locale} from 'svelte-i18n'
 	import {page} from '$app/stores'
 	import WhatsappEnquiry from '$lib/zoom/one-on-one/whatsapp-enquiry.svelte'
-
 	import VocabFeature from '$lib/zoom/one-on-one/vocab-feature.svelte'
+	import ImportantSharing from '$lib/zoom/one-on-one/important-sharing.svelte'
 	import AfterClassRevisionModule from '$lib/zoom/one-on-one/after-class-revision-module.svelte'
+	const slug = $page.path.split('/').pop()
 </script>
 
 <div class="mx-auto p-4 max-w-screen-lg">
-	<h1>補底增強(特製) 英文躍升計劃</h1>
+	<h1>{$_(slug)}</h1>
 	<img class="rounded my-4" src="https://ehla-media-bucket.s3.ap-southeast-1.amazonaws.com/website2021/one-on-one/{$page.path.split('/').pop()}.jpg" alt="banner">
-	<p>🎯 閱讀、寫作及對話主題教學之中，也會延伸教授內容及語境中的相關詞彙、文法(Grammar)、朗讀、發音及表達、閱讀理解技巧。過程當中也會不時滲入日常對話教學。</p>
-	<p>🎯 這個課堂另一寶貴的部分，在於老師會持續引導及教授學生就不同閱讀主題一對一進行互動和討論，除了拓寬閱讀視野，對提升學生英文說話及聆聽能力幫助甚大，非本地老師可以做到。</p>
-	<ul>
-		<li>⭐ 根據學生水平及需要調校內容及難度</li>
-		<li>⭐ 學生可以根據水平及需要，靈活選擇以下範疇(可選全部)：</li>
-		<li>🎯 閱讀＋說話 </li>
-		<li>🎯 閱讀理解技巧提升</li>
-		<li>🎯 寫作</li>
-		<li>🎯 文法應用</li>
-		<li>🎯 聆聽</li>
-	</ul>
+	{#if $locale === 'hk'}
+		<p>🎯 閱讀、寫作及對話主題教學之中，也會延伸教授內容及語境中的相關詞彙、文法(Grammar)、朗讀、發音及表達、閱讀理解技巧。過程當中也會不時滲入日常對話教學。</p>
+		<p>🎯 這個課堂另一寶貴的部分，在於老師會持續引導及教授學生就不同閱讀主題一對一進行互動和討論，除了拓寬閱讀視野，對提升學生英文說話及聆聽能力幫助甚大，非本地老師可以做到。</p>
+		<ul>
+			<li>⭐ 根據學生水平及需要調校內容及難度</li>
+			<li>⭐ 學生可以根據水平及需要，靈活選擇以下範疇(可選全部)：</li>
+			<li>🎯 閱讀＋說話 </li>
+			<li>🎯 閱讀理解技巧提升</li>
+			<li>🎯 寫作</li>
+			<li>🎯 文法應用</li>
+			<li>🎯 聆聽</li>
+		</ul>
+	{:else}
+		<p>🎯 The main learning focus is on reading, writing and speaking. Learning is extended to vocabulary building,  pronunciation and recitation, grammar and reading comprehension. In between the classes, daily life conversation and speaking sessions will be brought in to nurture children's ability to "speak like a native".</p>
+		<p>🎯 Another valuable part of this class is about offering the opportunity of one-on-one interactions and discussions with native speaker teachers on various reading topics. The goal of students is not just to broaden the reading horizons, but also try the best to train themselves up to enhance the level of speaking proficiency, so as to build up their competitive advantage in future study life.</p>
+		<p class="underline">Tailored content and difficulty level to fit the student's level and needs:</p>
+		<ul>
+			<li>🎯 Reading & Speaking</li>
+			<li>🎯 Reading Comprehension Skills</li>
+			<li>🎯 Writing</li>
+			<li>🎯 Grammar Usage</li>
+			<li>🎯 Listening Skills</li>
+		</ul>
+	{/if}
+
 	<div class="bg-yellow-100 p-2 my-4">
-		<div class="text-blue-500 font-bold">
-			❝一個同外籍老師學英文，聽及講咗1年英文的小朋友，與另一個無同等經歷的小朋友，分別是什麼？❞
-		</div>
-		<video class="max-w-full max-w-xl" controls src="https://ehla-media-bucket.s3.ap-southeast-1.amazonaws.com/website2021/story-telling.mp4"></video>
+		<ImportantSharing/>
 	</div>
 	<div class="bg-yellow-100 p-2 my-4">
 		<AfterClassRevisionModule/>
@@ -37,11 +50,15 @@
 		<VocabFeature/>
 	</div>
 	<div class="p-4 bg-gray-200 rounded">
-		<p>形式：一對一 Zoom</p>
-		<p>課時: 每節課30/40分鐘</p>
+		<li>{$_('lesson_format')}</li>
+		<li>{$_('lesson_duration_30_40')}</li>
 		<WhatsappEnquiry/>
 	</div>
 </div>
+
+<svelte:head>
+	<title>{$_(slug)}</title>
+</svelte:head>
 
 <style>
     h1 {
