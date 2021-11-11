bc5c546e7f13c454777f0e585624d0bb8fe2241e
cliff <cliff.wong@ehlacademy.org>
2021-11-11 11:11:30 +0800

    wip

--- src/routes/[locale]/zoom/one-on-one/story-telling-and-speaking.svelte
+++ src/routes/[locale]/zoom/one-on-one/story-telling-and-speaking.svelte
@@ -3,25 +3,29 @@
 </script>
 
 <script>
+	import {_, locale} from 'svelte-i18n'
 	import {page} from '$app/stores'
 	import AfterClassRevisionModule from '$lib/zoom/one-on-one/after-class-revision-module.svelte'
 	import VocabFeature from '$lib/zoom/one-on-one/vocab-feature.svelte'
+	import ImportantSharing from '$lib/zoom/one-on-one/important-sharing.svelte'
 	import WhatsappEnquiry from '$lib/zoom/one-on-one/whatsapp-enquiry.svelte'
+	const slug = $page.path.split('/').pop()
 </script>
 
 <div class="p-4">
 	<div class="mb-8">
-		<h1 class="text-t2 font-bold mb-1">英文故事閱讀與會話課堂</h1>
-		<p class="mb-4 text-p3">(L1-L4)</p>
+		<h1>{$_(slug)}</h1>
+		<p>(L1-L4)</p>
 		<img class="rounded my-4" src="https://ehla-media-bucket.s3.ap-southeast-1.amazonaws.com/website2021/one-on-one/{$page.path.split('/').pop()}.jpg" alt="banner">
-		<p class="text-p2">閱讀與英語會話是英文重要基礎! Zoom課堂提供了不可多得的純正英語環境及機會給孩子，在故事閱讀教學之中學習與外籍老師實時互動，不知不覺提升英文閱讀、聆聽和說話能力。這是香港本地老師和父母難以做到的!</p>
+		{#if $locale === 'hk'}
+			<p>閱讀與英語會話是英文重要基礎! Zoom課堂提供了不可多得的純正英語環境及機會給孩子，在故事閱讀教學之中學習與外籍老師實時互動，不知不覺提升英文閱讀、聆聽和說話能力。這是香港本地老師和父母難以做到的!</p>
+		{:else}
+			<p>Reading and speaking are important foundations of English language! The story reading class provides an excellent English speaking environment and opportunity for children to learn and interact with native speaker teachers, and improve their English reading, listening and speaking proficiency.</p>
+		{/if}
 	</div>
 
 	<div class="my-8">
-		<h2 class="font-bold text-t1 mb-2">分享</h2>
-		<p class="font-bold text-blue-500">❝一個同外籍老師學英文，聽及講咗1年英文的小朋友，與另一個無同等經歷的小朋友，分別是什麼？❞</p>
-		以下是視頻解答：
-		<video class="max-w-full max-w-xl" controls src="https://ehla-media-bucket.s3.ap-southeast-1.amazonaws.com/website2021/story-telling.mp4"></video>
+		<ImportantSharing/>
 	</div>
 
 	<div class="p-2 bg-yellow-100">
@@ -32,8 +36,24 @@
 		<VocabFeature/>
 	</div>
 	<div class="p-4 bg-gray-200 rounded">
-		<p>形式：一對一 Zoom</p>
-		<p>課時: 每節課30/40分鐘</p>
+		<li>{$_('lesson_format')}</li>
+		<li>{$_('lesson_duration_30_40')}</li>
 		<WhatsappEnquiry/>
 	</div>
 </div>
+
+<svelte:head>
+	<title>{$_(slug)}</title>
+</svelte:head>
+
+<style>
+    h1 {
+        @apply mb-4 font-bold text-t1;
+    }
+    p {
+        @apply mb-6 text-gray-700 leading-relaxed;
+    }
+    li {
+        @apply mb-1;
+    }
+</style>
