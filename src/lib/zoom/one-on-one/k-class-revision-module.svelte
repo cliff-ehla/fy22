8f7763c144e43f404839f540154c1a2ca3ef071b
cliff <cliff.wong@ehlacademy.org>
2021-11-11 14:26:15 +0800

    wip

--- src/lib/zoom/one-on-one/k-class-revision-module.svelte
+++ src/lib/zoom/one-on-one/k-class-revision-module.svelte
@@ -1,15 +1,15 @@
 <script>
 	import {_, locale} from 'svelte-i18n'
+	import Whatsapp from '$lib/zoom/one-on-one/whatsapp-enquiry.svelte'
 </script>
 
-<h2 class="text-t1 mb-2">{$_('k3_revision_pack.title')}</h2>
-<div role="doc-subtitle" class="leading-tight mb-4">{$_('k3_revision_pack.subtitle')}</div>
-<p>{$_('k3_revision_pack.desc')}</p>
-<p>{$_('k3_revision_pack.note')}</p>
-<video controls src="https://ehla-media-bucket.s3.ap-southeast-1.amazonaws.com/website2021/powerful-solution/video3.mp4"></video>
-
-<style>
-	p {
-			@apply leading-relaxed mb-4;
-	}
-</style>
+<div class="article">
+	<h2 class="mb-2">{$_('k3_revision_pack.title')}</h2>
+	<div role="doc-subtitle" class="leading-tight mb-4">{$_('k3_revision_pack.subtitle')}</div>
+	<p>{$_('k3_revision_pack.desc')}</p>
+	<div class="bg-brown-100 rounded p-2 text-sm">
+		<p>{$_('k3_revision_pack.note')}</p>
+		<Whatsapp/>
+	</div>
+	<video controls src="https://ehla-media-bucket.s3.ap-southeast-1.amazonaws.com/website2021/powerful-solution/video3.mp4"></video>
+</div>
--- src/lib/zoom/one-on-one/vocab-feature.svelte
+++ src/lib/zoom/one-on-one/vocab-feature.svelte
@@ -6,39 +6,26 @@
 	export let show_price
 </script>
 
-<h2>{$_('vocab_module.title')}</h2>
-<div role="doc-subtitle">{$_('vocab_module.subtitle')}</div>
-<ul>
-	{#each [1,2,3,4,5,6] as i}
-		<li>🎯 {$_(`vocab_module.p${i}`)}</li>
-	{/each}
-</ul>
-{#if show_price}
-	<div class="p-2 bg-gray-100" style="font-size: 15px">
-		<p class="underline">{$_('vocab_module.order.title')}</p>
-		<ul>
-			{#each [1,2] as i}
-				<li>{$_(`vocab_module.order.p${i}`)}</li>
-			{/each}
-		</ul>
-		<p>{$_('vocab_module.order.p3')}</p>
-		<Whatsapp powerful/>
-	</div>
-{/if}
-<video controls class="mb-2" src="https://ehla-media-bucket.s3.ap-southeast-1.amazonaws.com/website2021/powerful-solution/video_1.mp4"></video>
-<video controls src="https://ehla-media-bucket.s3.ap-southeast-1.amazonaws.com/website2021/powerful-solution/video2.mp4"></video>
-
-<style>
-    h2 {
-        @apply text-t1 text-black;
-    }
-    [role="doc-subtitle"] {
-		    @apply text-gray-400;
-    }
-    p, ul {
-        @apply my-4 text-gray-500;
-    }
-    li {
-        @apply mb-1 text-gray-500;
-    }
-</style>
+<div class="article">
+	<h2>{$_('vocab_module.title')}</h2>
+	<div role="doc-subtitle" class="mb-4">{$_('vocab_module.subtitle')}</div>
+	<ul>
+		{#each [1,2,3,4,5,6] as i}
+			<li>🎯 {$_(`vocab_module.p${i}`)}</li>
+		{/each}
+	</ul>
+	{#if show_price}
+		<div class="p-2 bg-brown-100 rounded" style="font-size: 15px">
+			<p class="underline">{$_('vocab_module.order.title')}</p>
+			<ul>
+				{#each [1,2] as i}
+					<li>{$_(`vocab_module.order.p${i}`)}</li>
+				{/each}
+			</ul>
+			<p>{$_('vocab_module.order.p3')}</p>
+			<Whatsapp powerful/>
+		</div>
+	{/if}
+	<video controls class="mb-2" src="https://ehla-media-bucket.s3.ap-southeast-1.amazonaws.com/website2021/powerful-solution/video_1.mp4"></video>
+	<video controls src="https://ehla-media-bucket.s3.ap-southeast-1.amazonaws.com/website2021/powerful-solution/video2.mp4"></video>
+</div>
