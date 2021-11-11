052211b544d75ec97d4dc0777fbe29221f580cd7
cliff <cliff.wong@ehlacademy.org>
2021-11-11 11:08:09 +0800

    wip

--- src/lib/zoom/one-on-one/exam-training-pack.svelte
+++ src/lib/zoom/one-on-one/exam-training-pack.svelte
@@ -1,5 +1,5 @@
 <script>
-	import {_} from 'svelte-i18n'
+	import {locale, _} from 'svelte-i18n'
 	import Icon from '$lib/ui/icon.svelte'
 </script>
 
@@ -7,6 +7,8 @@
 	<h2 class="font-bold">{$_('exam_training_pack.title')}</h2>
 	<p>{$_('exam_training_pack.subtitle')}</p>
 </div>
-<p class="mb-4">👉避免浪費錢❗ 浪費時間❗你需要睇睇以下短片。</p>
-<p class="mb-4">👉睇完呢條片，你會明白為什麼之前補習英文，補極都唔得嘅原因；以及要怎麼做，先可以有真正嘅成效？</p>
+{#if $locale === 'hk'}
+	<p class="mb-4">👉避免浪費錢❗ 浪費時間❗你需要睇睇以下短片。</p>
+	<p class="mb-4">👉睇完呢條片，你會明白為什麼之前補習英文，補極都唔得嘅原因；以及要怎麼做，先可以有真正嘅成效？</p>
+{/if}
 <video controls src="https://ehla-media-bucket.s3.ap-southeast-1.amazonaws.com/website2021/powerful-solution/video5.mp4"></video>
