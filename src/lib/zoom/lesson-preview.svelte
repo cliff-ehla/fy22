135496ce757fcaed3a7843ee91b2cde28389a2d5
cliff <cliff.wong@ehlacademy.org>
2021-11-12 19:16:40 +0800

    eric

--- src/lib/zoom/lesson-preview.svelte
+++ src/lib/zoom/lesson-preview.svelte
@@ -1,7 +1,8 @@
 <script>
 	export let item
 	import dayjs from "dayjs";
-	import {locale} from 'svelte-i18n'
+	import {locale, _} from 'svelte-i18n'
+	console.log(item)
 </script>
 
 <a href="/{$locale}/zoom/{item.rc_type}-{item.tutor_group_id}" class="flex">
@@ -10,13 +11,26 @@
 	</div>
 	<div class="ml-2 md:ml-4 flex flex-col sm:p-2">
 		<p class="text-blue-500 text-xs sm:text-sm sm:text">{item.rc_tag}</p>
-		<p class="font-bold text-sm sm:text">{$locale === 'hk' ? item.name_alter : item.name}</p>
-		<p class="text-gray-500 text-xs sm:text-sm mt-2">{item.rc_level}</p>
-		<p class="text-gray-500 text-xs sm:text-sm">{item.tutor_name}</p>
-		{#if item.start_date}
-			<div class="mt-auto">
-				<p class="mt-2 text-blue-500 text-sm sm:text">{dayjs(item.start_date).format('DD MMM YYYY HH:mma')}</p>
-			</div>
+		{#if item.name_alter}
+			<p class="font-bold text-sm sm:text">{$locale === 'hk' ? item.name_alter : item.name}</p>
 		{/if}
+		{#if item.rc_level}
+			<p class="font-bold text-sm sm:text">{$_('level')}: {item.rc_level}</p>
+		{/if}
+		<p class="text-gray-500 text-xs sm:text-sm mt-2">
+			{#if Number(item.student_size) === 9999}
+				{$_('unlimited_people_class')}
+			{:else if item.rc_type === 'big'}
+				{$_('20_people_class')}
+			{:else if item.rc_type === 'small'}
+				{$_('4_people_class')}
+			{/if}
+		</p>
+		<p class="text-gray-500 text-xs sm:text-sm">{$_('teacher')}: {item.tutor_name}</p>
+		<div class="mt-auto">
+			<p class="mt-2 text-blue-500 text-sm sm:text">
+				{item.start_date ? dayjs(item.start_date).format('DD MMM YYYY HH:mma') : $_('class_upon_request')}
+			</p>
+		</div>
 	</div>
 </a>
