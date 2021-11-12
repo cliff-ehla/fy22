d8759aff1059b774a77d46f018cd78504b6f9ad3
cliff <cliff.wong@ehlacademy.org>
2021-11-12 18:57:35 +0800

    trying sentry wip

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -10,6 +10,21 @@
 	import {navigating} from "$app/stores";
 	import {scale} from 'svelte/transition'
 
+	import * as Sentry from "@sentry/browser";
+	import { Integrations } from "@sentry/tracing";
+
+	Sentry.init({
+		dsn: "https://10f7700a46d645238b21377f73ec869f@o1067583.ingest.sentry.io/6061306",
+		integrations: [new Integrations.BrowserTracing()],
+
+		// Set tracesSampleRate to 1.0 to capture 100%
+		// of transactions for performance monitoring.
+		// We recommend adjusting this value in production
+		tracesSampleRate: 1.0,
+	});
+
+	myUndefinedFunction();
+
 	addMessages('en', en)
 	addMessages('hk', hk)
 
