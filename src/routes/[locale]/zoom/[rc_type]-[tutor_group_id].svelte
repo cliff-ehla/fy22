f4aa1a612e6dc861dd1eb3a5dd170c52dd113e82
cliff <cliff.wong@ehlacademy.org>
2021-11-23 18:01:25 +0800

    fix

--- src/routes/[locale]/zoom/[rc_type]-[tutor_group_id].svelte
+++ src/routes/[locale]/zoom/[rc_type]-[tutor_group_id].svelte
@@ -71,7 +71,7 @@
 <div class="bg-white max-w-screen-lg mx-auto p-4">
 	<p class="text-blue-500 font-bold text-t1">{$locale === 'hk' ? lesson.sub_cat_alter : lesson.sub_cat}</p>
 	<p class="font-bold md:text-xl">{lesson.name}</p>
-	<p class="text-gray-500 text-sm mt-2">{$_('level')}: {lesson.rc_level}</p>
+	<p class="text-gray-500 text-sm mt-2">{$_('level')}: {$_(lesson.rc_level)}</p>
 	<p class="text-gray-500 text-sm">{$_('duration')}: {lesson.duration} {$_('minutes')}</p>
 	<p class="text-gray-500 text-sm">{$_('teacher')}: {lesson.tutor_name}</p>
 	<div class="tag bg-blue-500">{lessonSizeLabel(lesson)}</div>
