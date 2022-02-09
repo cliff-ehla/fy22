d9a0217aff1601e116b407debc6a9911034b6e30
cliff <cliff.wong@ehlacademy.org>
2022-02-09 15:17:09 +0800

    qna

---
+++ src/lib/q&a/q&a-item.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let title
+</script>
+
+<div class="my-4 p-4 bg-white rounded border border-gray-300">
+	<div class="flex text-gray-800">
+		<div class="w-8 mb-2">Q：</div>
+		<p>{title}</p>
+	</div>
+	<div class="flex text-gray-500">
+		<div class="w-8">A：</div>
+		<div><slot/></div>
+	</div>
+</div>
